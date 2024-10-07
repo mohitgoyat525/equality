a561f36157da5d3bc5fb822bbafd6ba81b5ef393
mohitgoyat525 <montyits71@gmail.com>
2024-10-07 09:11:55 +0530

    remove-extraspace

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,28 +4,26 @@ function playAudio() {
   audio.onended = function () {
     audio.style.display = "none";
     document.getElementById("playButton").style.display = "inline";
-    document.getElementById("dropDown").style.display = "block"; // Show the text area
+    document.getElementById("dropDown").style.display = "block";
   };
   document.getElementById("playButton").style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Initialize event listener for the submit button
+  
   const form = document.querySelector("form");
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
 
     const textAreaValue = form.querySelector("textarea").value;
 
-    // Check the content of the textarea
+ 
     if (textAreaValue.trim() !== "") {
       alert(
         "If you believe in Equality thats nice"
       );
     } else {
       alert("In the End of the Day Equality is Just a Fantasy in (my opinion)");
-    };
-    
-    
+    }; 
   });
 });
